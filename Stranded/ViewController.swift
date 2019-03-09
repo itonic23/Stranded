69a53464bb6d19dacf1a6cbe1edf384d6343d370
Kanav Gupta <kanavgupta@Kanavs-MacBook-Pro.local>
2019-03-09 17:45:26 -0500

    Initial Commit

---
+++ Stranded/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stranded
+//
+//  Created by Kanav Gupta on 3/9/19.
+//  Copyright © 2019 trishi2002. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
