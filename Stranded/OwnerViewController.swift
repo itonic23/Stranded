93e8c24c715773bff67ecf7b33bc9e896a3e906e
Kanav Gupta <kanavgupta@kanavs-macbook-pro.local>
2019-03-10 12:24:52 -0400

    Initial Commit - SmartCar

---
+++ Stranded/OwnerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OwnerViewController.swift
+//  Stranded
+//
+//  Created by Kanav Gupta on 3/9/19.
+//  Copyright © 2019 trishi2002. All rights reserved.
+//
+
+import UIKit
+
+class OwnerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
